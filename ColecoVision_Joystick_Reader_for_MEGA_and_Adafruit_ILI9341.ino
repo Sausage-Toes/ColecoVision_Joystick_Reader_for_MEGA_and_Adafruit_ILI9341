43f572beba7d15f5b33caa51a49fdc28498fe5cf
Sausage-Toes <sausage.toos@gmail.com>
2015-11-19 21:10:31 -0600

    cleanup
    removed commented out code

--- ColecoVision_Joystick_Reader_for_MEGA_and_Adafruit_ILI9341.ino
+++ ColecoVision_Joystick_Reader_for_MEGA_and_Adafruit_ILI9341.ino
@@ -6,6 +6,7 @@
 #define TFT_DC 9
 #define TFT_CS 10
 
+//Define Input Pins on MEGA
 #define IN_PIN_1 22 //Brown UP
 #define IN_PIN_2 24 //Red DOWN
 #define IN_PIN_3 26 //Orange LEFT
@@ -18,6 +19,7 @@
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 uint8_t rotation=1;
+
 int h, w, x, y, xk, yk = 0;
 bool k1,k2,k3,k4,k5,k6,k7,k8,k9,k0,ka,kp = false;
 bool ok1,ok2,ok3,ok4,ok5,ok6,ok7,ok8,ok9,ok0,oka,okp = false;
@@ -32,22 +34,17 @@ void setup()
   h = tft.height();  w = tft.width();
   Serial.print("H:"); Serial.println(h);
   Serial.print("W:");Serial.println(w);
-  y = 60;
-  x = w/2 - 60;
-  xk = w/2 + 40;
-  yk = y-25;
 
   if (mode == 1)
   {
     InitializeController_Coleco();
-    DrawUI("Original Coleco", "n/c");
+    drawUI("Original Coleco", "n/c");
   }
   else if (mode == 2)
   {
     InitializeController_Coleco_Flashback();
-    DrawUI("Coleco Flashback", "n/c");
+    drawUI("Coleco Flashback", "n/c");
   }
-
 }
 
 void loop()
@@ -65,33 +62,17 @@ void loop()
   }
 }
 
-void outputPinStateToSerial()
-{  
-  Serial.print("1:");Serial.println(digitalRead(IN_PIN_1));
-  Serial.print("2:");Serial.println(digitalRead(IN_PIN_2)); 
-  Serial.print("3:");Serial.println(digitalRead(IN_PIN_3)); 
-  Serial.print("4:");Serial.println(digitalRead(IN_PIN_4)); 
-  Serial.print("5:");Serial.println(digitalRead(IN_PIN_5));
-  Serial.print("6:");Serial.println(digitalRead(IN_PIN_6)); 
-  Serial.print("7:");Serial.println(digitalRead(IN_PIN_7)); 
-  Serial.print("8:");Serial.println(digitalRead(IN_PIN_8)); 
-  Serial.print("9:");Serial.println(digitalRead(IN_PIN_9));  
-  Serial.println("====");
-}
-
 void InitializeController_Coleco_Flashback()
 {
   pinMode (IN_PIN_1, INPUT_PULLUP); // + pin2 low = RB
   pinMode (IN_PIN_2, INPUT_PULLUP); //up
-  //pinMode (IN_PIN_3, INPUT_PULLUP); // common always LOW?
+  pinMode (IN_PIN_3, OUTPUT); digitalWrite(IN_PIN_3, LOW); //Common
   pinMode (IN_PIN_4, INPUT_PULLUP); 
   pinMode (IN_PIN_5, INPUT_PULLUP); //down
   pinMode (IN_PIN_6, INPUT_PULLUP); //left
   pinMode (IN_PIN_7, INPUT_PULLUP); //right
   pinMode (IN_PIN_8, INPUT_PULLUP); // + pin2 low = LB
   pinMode (IN_PIN_9, INPUT_PULLUP); 
-
-  pinMode (IN_PIN_3, OUTPUT); digitalWrite(IN_PIN_3, LOW); //Common
 }
 
 void InitializeController_Coleco()
@@ -115,9 +96,6 @@ void ReadJoystick_Coleco_Flashback()
     if (digitalRead(IN_PIN_5) == LOW) drawDown(ILI9341_GREEN); else drawDown(ILI9341_BLACK);
     if (digitalRead(IN_PIN_6) == LOW) drawLeft(ILI9341_GREEN); else drawLeft(ILI9341_BLACK);
     if (digitalRead(IN_PIN_7) == LOW) drawRight(ILI9341_GREEN); else drawRight(ILI9341_BLACK);
-
-    Serial.println("=JOY=");
-    outputPinStateToSerial();
 }
 
 void ReadButtons_Coleco_Flashback()
@@ -125,9 +103,6 @@ void ReadButtons_Coleco_Flashback()
   pinMode (IN_PIN_2, OUTPUT); digitalWrite(IN_PIN_2, LOW);
   if (digitalRead(IN_PIN_1) == LOW) drawButton(-1,ILI9341_GREEN); else drawButton(-1,ILI9341_BLACK);
   if (digitalRead(IN_PIN_8) == LOW) drawButton(1,ILI9341_GREEN); else drawButton(1,ILI9341_BLACK);
-
-  //Serial.println("=BUTTONS=");
-  //outputPinStateToSerial();
   digitalWrite(IN_PIN_2, HIGH); pinMode (IN_PIN_2, INPUT_PULLUP);
 }
 
@@ -138,44 +113,24 @@ void ReadKeypad_Coleco_Flashback()
   k1 = digitalRead(IN_PIN_5) == LOW;
   k2 = digitalRead(IN_PIN_6) == LOW;
   k3 = digitalRead(IN_PIN_7) == LOW;
-  //if (digitalRead(IN_PIN_5) == LOW) drawKey (0,0,'1',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,0,'1',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_6) == LOW) drawKey (0,1,'2',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,1,'2',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_7) == LOW) drawKey (0,2,'3',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,2,'3',ILI9341_WHITE, ILI9341_BLACK);
-  Serial.println("=ROW1=");
-  outputPinStateToSerial();
   digitalWrite(IN_PIN_9, HIGH); pinMode (IN_PIN_9, INPUT_PULLUP); 
 
   pinMode (IN_PIN_4, OUTPUT); digitalWrite(IN_PIN_4, LOW); //k4 = pin5, k5 = 6, k6 = 7
   k4 = digitalRead(IN_PIN_5) == LOW;
   k5 = digitalRead(IN_PIN_6) == LOW;
   k6 = digitalRead(IN_PIN_7) == LOW;
-  //if (digitalRead(IN_PIN_5) == LOW) drawKey (1,0,'4',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,0,'4',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_6) == LOW) drawKey (1,1,'5',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,1,'5',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_7) == LOW) drawKey (1,2,'6',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,2,'6',ILI9341_WHITE, ILI9341_BLACK);
-  Serial.println("=ROW2=");
-  outputPinStateToSerial();
   digitalWrite(IN_PIN_4, HIGH); pinMode (IN_PIN_4, INPUT_PULLUP); 
   
   pinMode (IN_PIN_8, OUTPUT); digitalWrite(IN_PIN_8, LOW); // k7 = pin5, k8 = 6, k9 = 7
   k7 = digitalRead(IN_PIN_5) == LOW;
   k8 = digitalRead(IN_PIN_6) == LOW;
   k9 = digitalRead(IN_PIN_7) == LOW;
-  //if (digitalRead(IN_PIN_5) == LOW) drawKey (2,0,'7',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,0,'7',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_6) == LOW) drawKey (2,1,'8',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,1,'8',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_7) == LOW) drawKey (2,2,'9',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,2,'9',ILI9341_WHITE, ILI9341_BLACK);
-  Serial.println("=ROW3=");
-  outputPinStateToSerial();
   digitalWrite(IN_PIN_8, HIGH); pinMode (IN_PIN_8, INPUT_PULLUP); 
    
   pinMode (IN_PIN_1, OUTPUT); digitalWrite(IN_PIN_1, LOW); // ka = pin5, k0 = 6, kp = 7
   ka = digitalRead(IN_PIN_5) == LOW;
   k0 = digitalRead(IN_PIN_6) == LOW;
   kp = digitalRead(IN_PIN_7) == LOW;
-  //if (digitalRead(IN_PIN_5) == LOW) drawKey (3,0,'*',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,0,'*',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_6) == LOW) drawKey (3,1,'0',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,1,'0',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_7) == LOW) drawKey (3,2,'#',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,2,'#',ILI9341_WHITE, ILI9341_BLACK);
-  Serial.println("=ROW4=");
-  outputPinStateToSerial();
   digitalWrite(IN_PIN_1, HIGH); pinMode (IN_PIN_1, INPUT_PULLUP); 
 
   drawKeyPress();
@@ -185,12 +140,11 @@ void ReadKeypad_Coleco_Flashback()
 void ReadJoystick_Coleco()
 {
   digitalWrite(IN_PIN_5, HIGH); digitalWrite(IN_PIN_8, LOW); //Set Mode to READ JOYSTICK
-
-    if (digitalRead(IN_PIN_1) == LOW) drawUp(ILI9341_GREEN); else drawUp(ILI9341_BLACK);
-    if (digitalRead(IN_PIN_2) == LOW) drawDown(ILI9341_GREEN); else drawDown(ILI9341_BLACK);
-    if (digitalRead(IN_PIN_3) == LOW) drawLeft(ILI9341_GREEN); else drawLeft(ILI9341_BLACK);
-    if (digitalRead(IN_PIN_4) == LOW) drawRight(ILI9341_GREEN); else drawRight(ILI9341_BLACK);
-    if (digitalRead(IN_PIN_6) == LOW) drawButton(-1,ILI9341_GREEN); else drawButton(-1,ILI9341_BLACK);
+  if (digitalRead(IN_PIN_1) == LOW) drawUp(ILI9341_GREEN); else drawUp(ILI9341_BLACK);
+  if (digitalRead(IN_PIN_2) == LOW) drawDown(ILI9341_GREEN); else drawDown(ILI9341_BLACK);
+  if (digitalRead(IN_PIN_3) == LOW) drawLeft(ILI9341_GREEN); else drawLeft(ILI9341_BLACK);
+  if (digitalRead(IN_PIN_4) == LOW) drawRight(ILI9341_GREEN); else drawRight(ILI9341_BLACK);
+  if (digitalRead(IN_PIN_6) == LOW) drawButton(-1,ILI9341_GREEN); else drawButton(-1,ILI9341_BLACK);
 }
 
 void ReadKeypad_Coleco()
@@ -218,79 +172,6 @@ void ReadKeypad_Coleco()
 
   drawKeyPress();
 
-//  if (k1 != ok1)
-//  {
-//    if (k1) drawKey (0,0,'1',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,0,'1',ILI9341_WHITE, ILI9341_BLACK);
-//    ok1 = k1;
-//  }
-//  if (k2 != ok2)
-//  {
-//    if (k2) drawKey (0,1,'2',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,1,'2',ILI9341_WHITE, ILI9341_BLACK);
-//    ok2 = k2;
-//  }
-//  if (k3 != ok3)
-//  {
-//    if (k3) drawKey (0,2,'3',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,2,'3',ILI9341_WHITE, ILI9341_BLACK);
-//    ok3 = k3;
-//  }
-//  if (k4 != ok4)
-//  {
-//    if (k4) drawKey (1,0,'4',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,0,'4',ILI9341_WHITE, ILI9341_BLACK);
-//    ok4 = k4;
-//  } 
-//  if (k5 != ok5)
-//  {
-//    if (k5) drawKey (1,1,'5',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,1,'5',ILI9341_WHITE, ILI9341_BLACK);
-//    ok5 = k5;
-//  } 
-//  if (k6 != ok6)
-//  {
-//    if (k6) drawKey (1,2,'6',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,2,'6',ILI9341_WHITE, ILI9341_BLACK);
-//    ok6 = k6;
-//  } 
-//  if (k7 != ok7)
-//  {
-//    if (k7) drawKey (2,0,'7',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,0,'7',ILI9341_WHITE, ILI9341_BLACK);
-//    ok7 = k7;
-//  } 
-//  if (k8 != ok8)
-//  {
-//    if (k8) drawKey (2,1,'8',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,1,'8',ILI9341_WHITE, ILI9341_BLACK);
-//    ok8 = k8;
-//  } 
-//  if (k9 != ok9)
-//  {
-//    if (k9) drawKey (2,2,'9',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,2,'9',ILI9341_WHITE, ILI9341_BLACK);
-//    ok9 = k9;
-//  } 
-//  if (ka != oka)
-//  {
-//    if (ka) drawKey (3,0,'*',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,0,'*',ILI9341_WHITE, ILI9341_BLACK);
-//    oka = ka;
-//  } 
-//  if (k0 != ok0)
-//  {
-//    if (k0) drawKey (3,1,'0',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,1,'0',ILI9341_WHITE, ILI9341_BLACK);
-//    ok0 = k0;
-//  } 
-//  if (kp != okp)
-//  {
-//    if (kp) drawKey (3,2,'#',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,2,'#',ILI9341_WHITE, ILI9341_BLACK);
-//    okp = kp;
-//  }  
-  //if (digitalRead(IN_PIN_1) == HIGH && digitalRead(IN_PIN_2) == HIGH && digitalRead(IN_PIN_3) == HIGH && digitalRead(IN_PIN_4) == LOW) drawKey (0,0,'1',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,0,'1',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == HIGH && digitalRead(IN_PIN_2) == HIGH && digitalRead(IN_PIN_3) == LOW  && digitalRead(IN_PIN_4) == HIGH) drawKey (0,1,'2',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,1,'2',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == LOW  && digitalRead(IN_PIN_4) == LOW) drawKey (0,2,'3',ILI9341_BLACK, ILI9341_GREEN); else drawKey (0,2,'3',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == LOW  && digitalRead(IN_PIN_2) == LOW  && digitalRead(IN_PIN_3) == LOW) drawKey (1,0,'4',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,0,'4',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == HIGH && digitalRead(IN_PIN_2) == LOW  && digitalRead(IN_PIN_3) == LOW  && digitalRead(IN_PIN_4) == HIGH) drawKey (1,1,'5',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,1,'5',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == LOW  && digitalRead(IN_PIN_2) == HIGH && digitalRead(IN_PIN_3) == HIGH && digitalRead(IN_PIN_4) == HIGH) drawKey (1,2,'6',ILI9341_BLACK, ILI9341_GREEN); else drawKey (1,2,'6',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == HIGH && digitalRead(IN_PIN_2) == HIGH && digitalRead(IN_PIN_3) == LOW  && digitalRead(IN_PIN_4) == LOW) drawKey (2,0,'7',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,0,'7',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == HIGH && digitalRead(IN_PIN_2) == LOW  && digitalRead(IN_PIN_3) == LOW  && digitalRead(IN_PIN_4) == LOW) drawKey (2,1,'8',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,1,'8',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == HIGH && digitalRead(IN_PIN_2) == LOW  && digitalRead(IN_PIN_3) == HIGH && digitalRead(IN_PIN_4) == HIGH) drawKey (2,2,'9',ILI9341_BLACK, ILI9341_GREEN); else drawKey (2,2,'9',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == HIGH && digitalRead(IN_PIN_2) == LOW  && digitalRead(IN_PIN_3) == HIGH && digitalRead(IN_PIN_4) == LOW) drawKey (3,0,'*',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,0,'*',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == LOW  && digitalRead(IN_PIN_2) == LOW && digitalRead(IN_PIN_3) == HIGH) drawKey (3,1,'0',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,1,'0',ILI9341_WHITE, ILI9341_BLACK);
-  //if (digitalRead(IN_PIN_1) == LOW  && digitalRead(IN_PIN_2) == HIGH && digitalRead(IN_PIN_3) == LOW) drawKey (3,2,'#',ILI9341_BLACK, ILI9341_GREEN); else drawKey (3,2,'#',ILI9341_WHITE, ILI9341_BLACK);
-  
   if (digitalRead(IN_PIN_6) == LOW) drawButton(1,ILI9341_GREEN); else drawButton(1,ILI9341_BLACK);
 }
 
@@ -358,8 +239,15 @@ void drawKeyPress()
   }  
 }
 
-void DrawUI(String i, String o)
+void drawUI(String i, String o)
 {
+  //H:240
+  //W:320
+  y = 60;
+  x = w/2 - 60;
+  xk = w/2 + 40;
+  yk = y-25;
+  
   //UP
   tft.drawLine(x, y, x-20,  y+20, ILI9341_WHITE); // /
   tft.drawLine(x, y, x+20,  y+20, ILI9341_WHITE); // \
@@ -429,17 +317,7 @@ void DrawUI(String i, String o)
   tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
   tft.print("OUT:");
   tft.setTextColor(ILI9341_GREEN,ILI9341_BLACK);
-  tft.println(o);
-  
-
-  //BROKEN SCREEN LINE???
-//  tft.drawFastVLine(3,0,tft.height(),ILI9341_MAGENTA);
-//  tft.drawFastVLine(4,0,tft.height(),ILI9341_BLACK); //STUCK??
-//  tft.drawFastVLine(5,0,tft.height(),ILI9341_CYAN);
-//
-//  tft.drawFastVLine(15,0,tft.height(),ILI9341_RED);
-//  tft.drawFastVLine(16,0,tft.height(),ILI9341_BLACK); //STUCK??
-//  tft.drawFastVLine(17,0,tft.height(),ILI9341_BLUE);   
+  tft.println(o); 
 }
 
 void drawKey (int row, int col, char c, uint16_t color, uint16_t bg)
@@ -478,3 +356,16 @@ void drawRight (uint16_t color)
   tft.fillRect(x+21,y+51,20,19, color);
 }
 
+void outputPinStateToSerial()
+{  
+  Serial.print("1:");Serial.println(digitalRead(IN_PIN_1));
+  Serial.print("2:");Serial.println(digitalRead(IN_PIN_2)); 
+  Serial.print("3:");Serial.println(digitalRead(IN_PIN_3)); 
+  Serial.print("4:");Serial.println(digitalRead(IN_PIN_4)); 
+  Serial.print("5:");Serial.println(digitalRead(IN_PIN_5));
+  Serial.print("6:");Serial.println(digitalRead(IN_PIN_6)); 
+  Serial.print("7:");Serial.println(digitalRead(IN_PIN_7)); 
+  Serial.print("8:");Serial.println(digitalRead(IN_PIN_8)); 
+  Serial.print("9:");Serial.println(digitalRead(IN_PIN_9));  
+  Serial.println("====");
+}
