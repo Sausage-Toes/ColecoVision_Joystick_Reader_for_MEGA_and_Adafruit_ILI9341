e8cf4d3e76677b53ff6d8418154b6ccc4259cfe5
Sausage-Toes <sausage.toos@gmail.com>
2015-11-21 02:00:11 -0600

    Added Input Pin  Display

--- ColecoVision_Joystick_Reader_for_MEGA_and_Adafruit_ILI9341.ino
+++ ColecoVision_Joystick_Reader_for_MEGA_and_Adafruit_ILI9341.ino
@@ -1,7 +1,18 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include <Wire.h>
+#include <Adafruit_STMPE610.h>
 
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 150
+#define TS_MINY 130
+#define TS_MAXX 3800
+#define TS_MAXY 4000
+
+// The STMPE610 uses hardware SPI on the shield, and #8
+#define STMPE_CS 8
+Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
 // For the Adafruit shield, these are the default.
 #define TFT_DC 9
 #define TFT_CS 10
@@ -20,10 +31,15 @@
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 uint8_t rotation=1;
 
-int h, w, x, y, xk, yk = 0;
+int h, w, x, y, xk, yk, xt, yt = 0;
 bool k1,k2,k3,k4,k5,k6,k7,k8,k9,k0,ka,kp = false;
 bool ok1,ok2,ok3,ok4,ok5,ok6,ok7,ok8,ok9,ok0,oka,okp = false;
+bool p1,p2,p3,p4,p5,p6,p7,p8,p9 = false;
+bool op1,op2,op3,op4,op5,op6,op7,op8,op9 =false;
+bool showInputPins = false;
+int inputPinDisplay_x = 30, inputPinDisplay_y = 212;
 int mode = 2;
+bool input_initialized = false;
 
 void setup() 
 {
@@ -34,28 +50,34 @@ void setup()
   h = tft.height();  w = tft.width();
   Serial.print("H:"); Serial.println(h);
   Serial.print("W:");Serial.println(w);
-
-  if (mode == 1)
-  {
-    InitializeController_Coleco();
-    drawUI("Original Coleco", "n/c");
-  }
-  else if (mode == 2)
-  {
-    InitializeController_Coleco_Flashback();
-    drawUI("Coleco Flashback", "n/c");
-  }
+  if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
 }
 
 void loop()
 {
+  if (mode == 0)
+  {
+ 
+  }
   if (mode == 1)
   {
+    if (!input_initialized)
+    {
+      InitializeController_Coleco();
+      drawUI("Original Coleco", "n/c");
+      input_initialized = true;
+    }
     ReadJoystick_Coleco();
     ReadKeypad_Coleco();
   }
   else if (mode == 2)
   {
+    if (!input_initialized)
+    {
+      InitializeController_Coleco_Flashback();
+      drawUI("Coleco Flashback", "n/c");
+      input_initialized = true;
+    }
     ReadJoystick_Coleco_Flashback();
     ReadButtons_Coleco_Flashback();
     ReadKeypad_Coleco_Flashback();
@@ -67,6 +89,7 @@ void InitializeController_Coleco_Flashback()
   pinMode (IN_PIN_1, INPUT_PULLUP); // + pin2 low = RB
   pinMode (IN_PIN_2, INPUT_PULLUP); //up
   pinMode (IN_PIN_3, OUTPUT); digitalWrite(IN_PIN_3, LOW); //Common
+  p3 = false; op3 = true;
   pinMode (IN_PIN_4, INPUT_PULLUP); 
   pinMode (IN_PIN_5, INPUT_PULLUP); //down
   pinMode (IN_PIN_6, INPUT_PULLUP); //left
@@ -84,8 +107,8 @@ void InitializeController_Coleco()
   pinMode (IN_PIN_6, INPUT_PULLUP); //Blue BUTTON
 
   //Ground is switch between Pins 5 and 8 to mux Joystick with Keypad
-  // JOYSTICK = Pin 5 GREEN HIGH && Pin 8 GRAY LOW
-  // KEYPAD = Pin 5 GREEN LOW && Pin 8 GRAY HIGH
+  // JOYSTICK = Pin 5 == HIGH && Pin 8 == LOW
+  // KEYPAD = Pin 5 == LOW && Pin 8 == HIGH
   pinMode (IN_PIN_5, OUTPUT);
   pinMode (IN_PIN_8, OUTPUT);
 }
@@ -96,6 +119,7 @@ void ReadJoystick_Coleco_Flashback()
     if (digitalRead(IN_PIN_5) == LOW) drawDown(ILI9341_GREEN); else drawDown(ILI9341_BLACK);
     if (digitalRead(IN_PIN_6) == LOW) drawLeft(ILI9341_GREEN); else drawLeft(ILI9341_BLACK);
     if (digitalRead(IN_PIN_7) == LOW) drawRight(ILI9341_GREEN); else drawRight(ILI9341_BLACK);
+    if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
 }
 
 void ReadButtons_Coleco_Flashback()
@@ -103,36 +127,40 @@ void ReadButtons_Coleco_Flashback()
   pinMode (IN_PIN_2, OUTPUT); digitalWrite(IN_PIN_2, LOW);
   if (digitalRead(IN_PIN_1) == LOW) drawButton(-1,ILI9341_GREEN); else drawButton(-1,ILI9341_BLACK);
   if (digitalRead(IN_PIN_8) == LOW) drawButton(1,ILI9341_GREEN); else drawButton(1,ILI9341_BLACK);
+  if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
   digitalWrite(IN_PIN_2, HIGH); pinMode (IN_PIN_2, INPUT_PULLUP);
 }
 
 void ReadKeypad_Coleco_Flashback()
 {
-  digitalWrite(IN_PIN_3, HIGH);
-  pinMode (IN_PIN_9, OUTPUT); digitalWrite(IN_PIN_9, LOW); // k1 = pin5, k2 = 6, k3 = 7
+  digitalWrite(IN_PIN_3, HIGH); // mask joystick
+  pinMode (IN_PIN_9, OUTPUT); digitalWrite(IN_PIN_9, LOW); 
   k1 = digitalRead(IN_PIN_5) == LOW;
   k2 = digitalRead(IN_PIN_6) == LOW;
   k3 = digitalRead(IN_PIN_7) == LOW;
+    if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
   digitalWrite(IN_PIN_9, HIGH); pinMode (IN_PIN_9, INPUT_PULLUP); 
 
-  pinMode (IN_PIN_4, OUTPUT); digitalWrite(IN_PIN_4, LOW); //k4 = pin5, k5 = 6, k6 = 7
+  pinMode (IN_PIN_4, OUTPUT); digitalWrite(IN_PIN_4, LOW); 
   k4 = digitalRead(IN_PIN_5) == LOW;
   k5 = digitalRead(IN_PIN_6) == LOW;
   k6 = digitalRead(IN_PIN_7) == LOW;
+  if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
   digitalWrite(IN_PIN_4, HIGH); pinMode (IN_PIN_4, INPUT_PULLUP); 
   
-  pinMode (IN_PIN_8, OUTPUT); digitalWrite(IN_PIN_8, LOW); // k7 = pin5, k8 = 6, k9 = 7
+  pinMode (IN_PIN_8, OUTPUT); digitalWrite(IN_PIN_8, LOW); 
   k7 = digitalRead(IN_PIN_5) == LOW;
   k8 = digitalRead(IN_PIN_6) == LOW;
   k9 = digitalRead(IN_PIN_7) == LOW;
+  if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
   digitalWrite(IN_PIN_8, HIGH); pinMode (IN_PIN_8, INPUT_PULLUP); 
    
-  pinMode (IN_PIN_1, OUTPUT); digitalWrite(IN_PIN_1, LOW); // ka = pin5, k0 = 6, kp = 7
+  pinMode (IN_PIN_1, OUTPUT); digitalWrite(IN_PIN_1, LOW); 
   ka = digitalRead(IN_PIN_5) == LOW;
   k0 = digitalRead(IN_PIN_6) == LOW;
   kp = digitalRead(IN_PIN_7) == LOW;
+  if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
   digitalWrite(IN_PIN_1, HIGH); pinMode (IN_PIN_1, INPUT_PULLUP); 
-
   drawKeyPress();
   digitalWrite(IN_PIN_3, LOW);
 }
@@ -145,6 +173,7 @@ void ReadJoystick_Coleco()
   if (digitalRead(IN_PIN_3) == LOW) drawLeft(ILI9341_GREEN); else drawLeft(ILI9341_BLACK);
   if (digitalRead(IN_PIN_4) == LOW) drawRight(ILI9341_GREEN); else drawRight(ILI9341_BLACK);
   if (digitalRead(IN_PIN_6) == LOW) drawButton(-1,ILI9341_GREEN); else drawButton(-1,ILI9341_BLACK);
+  if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
 }
 
 void ReadKeypad_Coleco()
@@ -173,6 +202,7 @@ void ReadKeypad_Coleco()
   drawKeyPress();
 
   if (digitalRead(IN_PIN_6) == LOW) drawButton(1,ILI9341_GREEN); else drawButton(1,ILI9341_BLACK);
+  if (showInputPins) drawInputPins(inputPinDisplay_x,inputPinDisplay_y);
 }
 
 void drawKeyPress()
@@ -243,46 +273,49 @@ void drawUI(String i, String o)
 {
   //H:240
   //W:320
-  y = 60;
+  y = 80;
   x = w/2 - 60;
   xk = w/2 + 40;
   yk = y-25;
-  
+  xt = 50;
+  //yt = h-35;
+   yt = 3;
+   
   //UP
-  tft.drawLine(x, y, x-20,  y+20, ILI9341_WHITE); // /
-  tft.drawLine(x, y, x+20,  y+20, ILI9341_WHITE); // \
-  tft.drawFastHLine(x-20,y+20,10, ILI9341_WHITE);// _
-  tft.drawFastHLine(x+10,y+20,10, ILI9341_WHITE);// _
-  tft.drawFastVLine(x-10,y+20,20,ILI9341_WHITE); // |
-  tft.drawFastVLine(x+10,y+20,20,ILI9341_WHITE); // |
-  tft.drawFastHLine(x-10,y+40,20, ILI9341_WHITE); // _
+  tft.drawLine(x, y, x-20,  y+20, ILI9341_WHITE); 
+  tft.drawLine(x, y, x+20,  y+20, ILI9341_WHITE); 
+  tft.drawFastHLine(x-20,y+20,10, ILI9341_WHITE);
+  tft.drawFastHLine(x+10,y+20,10, ILI9341_WHITE);
+  tft.drawFastVLine(x-10,y+20,20,ILI9341_WHITE); 
+  tft.drawFastVLine(x+10,y+20,20,ILI9341_WHITE); 
+  tft.drawFastHLine(x-10,y+40,20, ILI9341_WHITE); 
   drawUp(ILI9341_BLACK);
   //DOWN
-  tft.drawLine(x, y+120, x-20,  y+100, ILI9341_WHITE); // /
-  tft.drawLine(x, y+120, x+20,  y+100, ILI9341_WHITE); // \
-  tft.drawFastHLine(x-20,y+100,10, ILI9341_WHITE);// _
-  tft.drawFastHLine(x+10,y+100,10, ILI9341_WHITE);// _
-  tft.drawFastVLine(x-10,y+80,20,ILI9341_WHITE); // |
-  tft.drawFastVLine(x+10,y+80,20,ILI9341_WHITE); // |
-  tft.drawFastHLine(x-10,y+80,20, ILI9341_WHITE); // _
+  tft.drawLine(x, y+120, x-20,  y+100, ILI9341_WHITE); 
+  tft.drawLine(x, y+120, x+20,  y+100, ILI9341_WHITE); 
+  tft.drawFastHLine(x-20,y+100,10, ILI9341_WHITE);
+  tft.drawFastHLine(x+10,y+100,10, ILI9341_WHITE);
+  tft.drawFastVLine(x-10,y+80,20,ILI9341_WHITE); 
+  tft.drawFastVLine(x+10,y+80,20,ILI9341_WHITE);
+  tft.drawFastHLine(x-10,y+80,20, ILI9341_WHITE); 
   drawDown(ILI9341_BLACK);
   //LEFT
-  tft.drawLine(x-60, y+60, x-40,  y+40, ILI9341_WHITE); // /
-  tft.drawLine(x-60, y+60, x-40,  y+80, ILI9341_WHITE); // \
-  tft.drawFastVLine(x-40,y+40,10,ILI9341_WHITE); // |
-  tft.drawFastVLine(x-40,y+70,10,ILI9341_WHITE); // |
-  tft.drawFastHLine(x-40,y+50,20,ILI9341_WHITE); // _
-  tft.drawFastHLine(x-40,y+70,20,ILI9341_WHITE); // _
-  tft.drawFastVLine(x-20,y+50,20, ILI9341_WHITE); // |
+  tft.drawLine(x-60, y+60, x-40,  y+40, ILI9341_WHITE); 
+  tft.drawLine(x-60, y+60, x-40,  y+80, ILI9341_WHITE); 
+  tft.drawFastVLine(x-40,y+40,10,ILI9341_WHITE); 
+  tft.drawFastVLine(x-40,y+70,10,ILI9341_WHITE); 
+  tft.drawFastHLine(x-40,y+50,20,ILI9341_WHITE); 
+  tft.drawFastHLine(x-40,y+70,20,ILI9341_WHITE); 
+  tft.drawFastVLine(x-20,y+50,20, ILI9341_WHITE); 
   drawLeft(ILI9341_BLACK);
   //RIGHT
-  tft.drawLine(x+60, y+60, x+40,  y+40, ILI9341_WHITE); // \
-  tft.drawLine(x+60, y+60, x+40,  y+80, ILI9341_WHITE); // /
-  tft.drawFastVLine(x+40,y+40,10,ILI9341_WHITE); // |
-  tft.drawFastVLine(x+40,y+70,10,ILI9341_WHITE); // |
-  tft.drawFastHLine(x+20,y+50,21,ILI9341_WHITE); // _
-  tft.drawFastHLine(x+20,y+70,20,ILI9341_WHITE); // _
-  tft.drawFastVLine(x+20,y+50,20, ILI9341_WHITE); // |
+  tft.drawLine(x+60, y+60, x+40,  y+40, ILI9341_WHITE); 
+  tft.drawLine(x+60, y+60, x+40,  y+80, ILI9341_WHITE); 
+  tft.drawFastVLine(x+40,y+40,10,ILI9341_WHITE); 
+  tft.drawFastVLine(x+40,y+70,10,ILI9341_WHITE);
+  tft.drawFastHLine(x+20,y+50,21,ILI9341_WHITE); 
+  tft.drawFastHLine(x+20,y+70,20,ILI9341_WHITE); 
+  tft.drawFastVLine(x+20,y+50,20, ILI9341_WHITE); 
   drawRight(ILI9341_BLACK);
   //middle
   tft.drawCircle(x,y+60,18, ILI9341_WHITE);
@@ -306,14 +339,14 @@ void drawUI(String i, String o)
   drawKey (3,0,'*',ILI9341_WHITE, ILI9341_BLACK);
   drawKey (3,1,'0',ILI9341_WHITE, ILI9341_BLACK);
   drawKey (3,2,'#',ILI9341_WHITE, ILI9341_BLACK);
-
-  tft.setCursor(50,h-40);
+  
+  tft.setCursor(xt,yt);
   tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
   tft.setTextSize(2);
   tft.print(" IN:");
   tft.setTextColor(ILI9341_GREEN,ILI9341_BLACK);
   tft.println(i);
-  tft.setCursor(50,h-20);
+  tft.setCursor(xt,yt+20);
   tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
   tft.print("OUT:");
   tft.setTextColor(ILI9341_GREEN,ILI9341_BLACK);
@@ -354,6 +387,108 @@ void drawRight (uint16_t color)
 {
   tft.fillTriangle(x+59,y+60,x+41,y+42,x+41,y+78, color);
   tft.fillRect(x+21,y+51,20,19, color);
+}
+
+void drawInputPins(int x0, int y0)
+{
+    int x = x0, y = y0, pad = 22;
+    
+    p1 = digitalRead(IN_PIN_1);
+    p2 = digitalRead(IN_PIN_2);
+    p3 = digitalRead(IN_PIN_3);
+    p4 = digitalRead(IN_PIN_4);
+    p5 = digitalRead(IN_PIN_5);
+    p6 = digitalRead(IN_PIN_6);
+    p7 = digitalRead(IN_PIN_7);
+    p8 = digitalRead(IN_PIN_8);    
+    p9 = digitalRead(IN_PIN_9);
+
+    x=x+pad;
+    if (p1 != op1)
+    {
+      if (!p1) drawPin(x,y,'1',ILI9341_WHITE, 0x8200); //brown = 0x8200
+      else  drawPin(x,y,'1',ILI9341_WHITE, ILI9341_BLACK);
+      op1  = p1;
+    }
+    
+    x=x+pad;
+    if (p2 != op2)
+    {
+      if (!p2) drawPin(x,y,'2',ILI9341_WHITE, ILI9341_RED); 
+      else  drawPin(x,y,'2',ILI9341_WHITE, ILI9341_BLACK);
+      op2  = p2;
+    }
+    //drawPin(x,y,'2',ILI9341_WHITE, ILI9341_RED); 
+
+    x=x+pad;
+    if (p3 != op3)
+    {
+      if (!p3) drawPin(x,y,'3',ILI9341_WHITE, ILI9341_ORANGE); 
+      else  drawPin(x,y,'3',ILI9341_WHITE, ILI9341_BLACK);
+      op3  = p3;
+    }
+    //drawPin(x,y,'3',ILI9341_WHITE, ILI9341_ORANGE);    
+
+    x=x+pad;
+    if (p4 != op4)
+    {
+      if (!p4) drawPin(x,y,'4',ILI9341_BLACK, ILI9341_YELLOW); 
+      else  drawPin(x,y,'4',ILI9341_WHITE, ILI9341_BLACK);
+      op4  = p4;
+    }
+    //drawPin(x,y,'4',ILI9341_BLACK, ILI9341_YELLOW);   
+    
+    x=x+pad;
+    if (p5 != op5)
+    {
+      if (!p5) drawPin(x,y,'5',ILI9341_BLACK, ILI9341_DARKGREEN); 
+      else  drawPin(x,y,'5',ILI9341_WHITE, ILI9341_BLACK);
+      op5  = p5;
+    }
+    //drawPin(x,y,'5',ILI9341_WHITE,ILI9341_DARKGREEN); 
+    
+    x=x0+pad+11; y=y0+18;
+    if (p6 != op6)
+    {
+      if (!p6) drawPin(x,y,'6',ILI9341_BLACK, ILI9341_NAVY); 
+      else  drawPin(x,y,'6',ILI9341_WHITE, ILI9341_BLACK);
+      op6  = p6;
+    }
+    //drawPin(x,y,'6',ILI9341_WHITE, ILI9341_NAVY);
+
+    x=x+pad;
+    if (p7 != op7)
+    {
+      if (!p7) drawPin(x,y,'7',ILI9341_BLACK, ILI9341_PURPLE); 
+      else  drawPin(x,y,'7',ILI9341_WHITE, ILI9341_BLACK);
+      op7  = p7;
+    }
+    //drawPin(x,y,'7',ILI9341_WHITE, ILI9341_PURPLE);
+
+    x=x+pad;
+    if (p8 != op8)
+    {
+      if (!p8) drawPin(x,y,'8',ILI9341_BLACK, ILI9341_DARKGREY); 
+      else  drawPin(x,y,'8',ILI9341_WHITE, ILI9341_BLACK);
+      op8  = p8;
+    }
+    //drawPin(x,y,'8',ILI9341_WHITE,  ILI9341_DARKGREY);
+    
+    x=x+pad;
+    if (p9 != op9)
+    {
+      if (!p9) drawPin(x,y,'9',ILI9341_BLACK,  ILI9341_WHITE); 
+      else  drawPin(x,y,'9',ILI9341_WHITE, ILI9341_BLACK);
+      op9  = p9;
+    }
+    //drawPin(x,y,'9',ILI9341_BLACK,  ILI9341_WHITE);  
+}
+
+void drawPin (int x, int y, char c, uint16_t color, uint16_t bg)
+{
+  tft.drawCircle(x,y, 8, ILI9341_WHITE);
+  tft.fillCircle(x,y,7,bg);
+  tft.drawChar(x-2, y-3, c, color, bg, 1);
 }
 
 void outputPinStateToSerial()
